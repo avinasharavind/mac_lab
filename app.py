2e6cbdff8243adc2737b8244d9b1a5da78111e9d
avinasharavind <aviaravind15@gmail.com>
2026-04-27 15:01:57 -0400

    Minor fix

--- app.py
+++ app.py
@@ -355,7 +355,7 @@ def fetch_hrrr_frames():
             print(f"[hrrr] Already up to date. Skipping.")
             return
         else:
-            os.remove(f"{CACHE_DIR}/hrrr_surface/*.png")
+            os.remove(f"{CACHE_DIR}/hrrr_surface/")
 
     # Pass the path to each render call
     print("[hrrr-surface] Entering Loop")
